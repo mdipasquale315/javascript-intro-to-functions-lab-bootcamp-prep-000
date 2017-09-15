750958f8d7e6814c94a7af33717492d4e9e7e876
Michael DiPasquale <mdipasquale315@gmail.com>
2017-09-15 21:57:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,7 @@ function logShout(string){
 function logWhisper(string){
   console.log(string.toLowerCase())
 }
+function sayHiToGrandma(string){
+  return "I can't hear you!"
+  if string.toLowerCase()
+}
